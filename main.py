b4bc9397e290bb1ea2a67e30ea101fad033d8021
martinatoffoletto <99765115+martinatoffoletto@users.noreply.github.com>
2025-07-04 18:32:25 -0300

    UPDATE

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import duckdb
 
 def run_command(cmd):
     print(f"Ejecutando: {cmd}")
@@ -14,3 +15,17 @@ if __name__ == "__main__":
     run_command("dbt seed")   
     run_command("dbt run")
     print("Pipeline completado correctamente.")
+    
+    
+    ###PARA VERIFICAR LA TABLA FINAL CARGADA EN DUCKDB###
+    print("\nVerificando tabla final 'final_combustible_ipc'...\n")
+    con = duckdb.connect("duckdb.db")  
+
+    try:
+        df = con.execute("SELECT * FROM final_combustible_ipc LIMIT 5").fetchdf()
+        print(df)
+        print("\n✔ Verificación exitosa: tabla cargada correctamente.")
+    except Exception as e:
+        print(f"\n Error al consultar la tabla final: {e}")
+    
+
